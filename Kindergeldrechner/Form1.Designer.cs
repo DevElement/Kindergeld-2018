49553336f02d7b20cec8b35912bc1a1bdbc64d42
Harald Rieder <hprieder@web.de>
2018-09-07 10:29:42 +0200

    First

---
+++ Kindergeldrechner/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Kindergeldrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.choosechild = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labchild = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // choosechild
+            // 
+            this.choosechild.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.choosechild.Cursor = System.Windows.Forms.Cursors.Help;
+            this.choosechild.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.choosechild.DropDownWidth = 23;
+            this.choosechild.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.choosechild.ForeColor = System.Drawing.Color.White;
+            this.choosechild.FormattingEnabled = true;
+            this.choosechild.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.choosechild.Location = new System.Drawing.Point(14, 38);
+            this.choosechild.Name = "choosechild";
+            this.choosechild.Size = new System.Drawing.Size(23, 23);
+            this.choosechild.TabIndex = 0;
+            this.choosechild.Tag = "";
+            this.choosechild.SelectedIndexChanged += new System.EventHandler(this.choosechild_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Anzahl der Kinder";
+            // 
+            // labchild
+            // 
+            this.labchild.AutoSize = true;
+            this.labchild.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labchild.ForeColor = System.Drawing.Color.Chocolate;
+            this.labchild.Location = new System.Drawing.Point(7, 95);
+            this.labchild.Name = "labchild";
+            this.labchild.Size = new System.Drawing.Size(0, 16);
+            this.labchild.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Kindergeldrechner.Properties.Resources.D;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.pictureBox1.Location = new System.Drawing.Point(486, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 58);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(431, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "© DevElement 2018 - Harald Rieder";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(555, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "help?";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(593, 237);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labchild);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.choosechild);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kindergeldrechner 2018";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox choosechild;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labchild;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Kindergeldrechner/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kindergeldrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void choosechild_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(this.choosechild.SelectedIndex)
+            {
+                case 0:
+                    this.labchild.Text = "Sie erhalten 0 EUR Kindergeld, da Sie keine Kinder haben.";
+                    break;
+                case 1:
+                    this.labchild.Text = "Sie erhalten für Ihr Kind 194 EUR Kindergeld.";
+                    break;
+                case 2:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden Kinder jeweils 194 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 388 EUR.";
+                    break;
+                case 3:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden ersten Kinder jeweils 194 EUR." + Environment.NewLine + "Für Ihr drittes Kind 200 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 588 EUR Kindergeld.\n";
+                    break;
+                case 4:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden ersten Kinder jeweils 194 EUR." + Environment.NewLine + "Für Ihr drittes Kind 200 EUR und für das vierte Kind 225 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 813 EUR Kindergeld.";
+                    break;
+                case 5:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden ersten Kinder jeweils 194 EUR." + Environment.NewLine + "Für Ihr drittes Kind 200 EUR und für das vierte und fünfte Kind jeweils 225 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 1.038 EUR Kindergeld.";
+                    break;
+                case 6:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden ersten Kinder jeweils 194 EUR." + Environment.NewLine + "Für Ihr drittes Kind 200 EUR und für das vierte, fünfte und sechste Kind jeweils 225 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 1.263 EUR Kindergeld.";
+                    break;
+                case 7:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden ersten Kinder jeweils 194 EUR." + Environment.NewLine + "Für Ihr drittes Kind 200 EUR und für das vierte bis siebente Kind jeweils 225 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 1.488 EUR Kindergeld.";
+                    break;
+                case 8:
+                    this.labchild.Text = "Sie erhalten für Ihre beiden ersten Kinder jeweils 194 EUR." + Environment.NewLine + "Für Ihr drittes Kind 200 EUR und für das vierte bis achte Kind jeweils 225 EUR Kindergeld." + Environment.NewLine + "Insgesamt bekommen Sie pro Monat 1.713 EUR Kindergeld.";
+                    break;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.develement.de");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.develement.de");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.develement.de");
+        }
+    }
+}
